dbb20de6e4c755c0a865ed496b1815a91782c3b9
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-05-07 17:54:52 +0900

    setup vue project with docker

---
+++ README.md
@@ -0,0 +1,8 @@
+# create app
+
+yarn create vite --template=vue
+
+# eslint + prettier
+
+yarn add -D eslint eslint-plugin-vue \
+ prettier @vue/eslint-config-prettier @rushstack/eslint-patch
---
+++ workspace/basics/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const title = 'The Final Empire'
+  const author = 'Brandon Sanderson'
+  const age = ref(45)
+
+  const increaseAge = () => {
+    age.value++
+  }
+  const decreaseAge = () => {
+    age.value--
+  }
+</script>
+
+<template>
+  <p>{{ title }} - {{ author }} - {{ age }}</p>
+  <button @click="increaseAge">Increase Age</button>
+  &nbsp;
+  <button @click="decreaseAge">Decrease Age</button>
+</template>
+
+<style></style>
