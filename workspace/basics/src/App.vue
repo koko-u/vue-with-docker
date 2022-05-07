a1732e5a5eea4d826459a88cd627fbd00341cd60
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-05-08 01:47:11 +0900

    step 17

--- workspace/basics/src/App.vue
+++ workspace/basics/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
   import { getImg } from './utils/getImg'
 
   const books = ref([
@@ -7,18 +7,24 @@
       title: 'name of the wind',
       author: 'patrick rothfuss',
       image: getImg('1.jpg'),
+      isFav: true,
     },
     {
       title: 'the way of kings',
       author: 'bardon sanderson',
       image: getImg('2.jpg'),
+      isFav: false,
     },
     {
       title: 'the final empire',
       author: 'bardon sanderson',
       image: getImg('3.jpg'),
+      isFav: true,
     },
   ])
+  const favoriteBooks = computed(() => {
+    return books.value.filter((book) => book.isFav)
+  })
 
   const showBook = ref(true)
   const toggle = () => {
@@ -26,36 +32,74 @@
   }
 
   const url = ref('http://www.thenetninja.co.uk')
+
+  const toggleFav = (book) => {
+    book.isFav = !book.isFav
+  }
 </script>
 
 <template>
-  <div>
-    <button @click="toggle">
-      {{ showBook ? 'Show' : 'Hidden' }}
-    </button>
-  </div>
-  <div v-if="showBook">
-    <ul>
-      <li v-for="book in books" :key="book.title">
-        <img :src="book.image" :alt="book.title" />
-        <h3>{{ book.title }}</h3>
-        <p>
-          {{ book.author }}
-        </p>
-      </li>
-    </ul>
-  </div>
+  <div class="board">
+    <div>
+      <button @click="toggle">
+        {{ showBook ? 'Show' : 'Hidden' }}
+      </button>
+    </div>
+    <div v-if="showBook">
+      <ul>
+        <li
+          v-for="book in favoriteBooks"
+          :key="book.title"
+          :class="{ fav: book.isFav }"
+          @click="toggleFav(book)"
+        >
+          <img :src="book.image" :alt="book.title" />
+          <h3>{{ book.title }}</h3>
+          <p>
+            {{ book.author }}
+          </p>
+        </li>
+      </ul>
+    </div>
 
-  <a :href="url">Best website ever.</a>
+    <a :href="url">Best website ever.</a>
+  </div>
 </template>
 
 <style scoped>
-  .box {
-    padding: 100px 0;
-    width: 400px;
-    text-align: center;
-    background: #ddd;
-    margin: 20px;
-    display: inline-block;
+  .board {
+    background: #eee;
+    max-width: 960px;
+    margin: 20px auto;
+  }
+
+  p,
+  h3,
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style-type: none;
+    background: #fff;
+    margin: 20px 10px;
+    padding: 10px 20px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  li.fav {
+    background: #ff9ed2;
+    color: #fff;
+  }
+
+  li:hover {
+    cursor: pointer;
+  }
+
+  .fav {
   }
 </style>
