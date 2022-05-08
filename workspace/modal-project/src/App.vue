4bcfc02e463de612598d1e3034b30b75c72242e6
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2022-05-08 15:28:15 +0900

    setup modal-project

---
+++ workspace/modal-project/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const title = 'My first vue app ;-'
+</script>
+
+<template>
+  <h1>{{ title }}...</h1>
+</template>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
+</style>
